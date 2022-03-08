0417f8e1c9c47105c8f126f776eed27e686d0fbe
Tarin Noovare <tarin.no@tnis.com>
2022-03-09 00:10:25 +0700

    Add footer note

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
 import MainBox from "./components/MainBox";
+import Footer from "./components/Footer";
 
 function App() {
-
-  const booksList = {
-    "History": ["Book1", "Book2", "Book3", "Book4", "Book5", "Book6", "Book7"],
-    "Non-Fiction": ["Book1", "Book2", "Book3", "Book4", "Book5"],
-    "Fiction": ["Book1", "Book2", "Book3", "Book4", "Book5", "Book6", "Book7", "Book8"],
-    "Science": ["Book1", "Book2", "Book3", "Book4", "Book5", "Book6", "Book7", "Book8", "Book9"]
-  }
   return (
     <div className="App">
-      <div className="header-title">Welcome Everyone To My Humble Books Archive</div>
+      <div className="header-title">
+        Welcome Everyone To My Humble Books Archive
+      </div>
       <MainBox />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="my-footer">
+        <div className="footer-content">
+        <p>Copyright © 2022 Tarin Noovare - All Rights Reserved </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
